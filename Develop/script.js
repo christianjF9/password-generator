fafebeef455414612ae1cc19a8984f0fd09acb3c
Christian Foote <christianfoote999@gmail.com>
2022-03-11 22:38:45 -0500

    added prompts for when incompatible choices have been chosen

--- Develop/script.js
+++ Develop/script.js
@@ -4,31 +4,39 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 const alphabet = "abcdefghijklmnopqrstuvwxyz";
-const specialcharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"';
+const specialcharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~" + String.fromCodePoint(92,34);;
 const numbers = "0123456789";
 
 function generatePassword(){
   var length = askLength();
-  var password ="";
-//0 to not include uppercase 0.5 to include
-  var lowercaseThreshold = 0;
-//1 to not include uppercase 0.5 to include
-var uppercaseThreshold = 0.5;
-//generate possible characters
-var characterPool = "";
-if(window.confirm("Should the password contain letters?")){
-  characterPool += alphabet;
-
-  if(window.confirm("Should the password contain lowercase letters?"))
-    lowercaseThreshold = 0.5;
-  if(window.confirm("Should the password contain uppercase letters?"))
-   uppercaseThreshold = 1;
-}
-if(window.confirm("Should the password contain special characters?"))
-  characterPool+=specialcharacters;
-if(window.confirm("Should the password contain numbers?"))
-  characterPool+=numbers;
+  //generate possible characters
+  var characterPool = "";
+  while (characterPool == ""){
+   //0 to not include uppercase 0.5 to include
+     var lowercaseThreshold = 0;
+    //1 to not include uppercase 0.5 to include
+   var uppercaseThreshold = 0.5;
+ 
+    if(window.confirm("Should the password contain letters?")){
+      characterPool += alphabet;
+      while(lowercaseThreshold == 0 && uppercaseThreshold==0.5){
+        if(window.confirm("Should the password contain lowercase letters?"))
+          lowercaseThreshold = 0.5;
+        if(window.confirm("Should the password contain uppercase letters?"))
+          uppercaseThreshold = 1;
+        else if(lowercaseThreshold == 0&& uppercaseThreshold==0.5)
+          window.alert("please pick uppercase lowercase or both");
+      }
+    }
+    if(window.confirm("Should the password contain special characters?"))
+      characterPool+=specialcharacters;
+    if(window.confirm("Should the password contain numbers?"))
+      characterPool+=numbers;
+    else if(characterPool = "")
+      window.alert("please pick at least one set of characters to generate your password")
+  }
 //fills password with possible characters
+  var password ="";
   for(i=0;i<length;i++){
     var charPos = Math.floor(Math.random() * characterPool.length);
     var newChar = characterPool.charAt(charPos);
@@ -54,7 +62,11 @@ function writePassword() {
 
 function askLength(){
   length = prompt("How long do you want your password? (must be between 8 and 128 characters)");
-  if (length<8||length>128){
+  console.log(length.is);
+  if (isNaN(length)){
+    alert("Please enter a number");
+    length = askLength();
+  } else if (length<8||length>128){
     alert("Password must be between 8 and 128 characters!")
     length = askLength();
   }
